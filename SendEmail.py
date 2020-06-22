0f92e398ecd01497e55846d690e7c1a6b487c876
herewego321 <43502056+herewego321@users.noreply.github.com>
2020-06-22 15:05:58 -0400

    Add files via upload

---
+++ SendEmail.py
@@ -0,0 +1,27 @@
+import smtplib, ssl
+
+import smtplib, ssl
+
+def send_email(mess):
+    """
+    mess:as the following format "subject/n/nEmail content"
+    """
+    port = 587  # For starttls
+    smtp_server = "smtp.gmail.com"
+    sender_email = "senderemail@gmail.com"
+    receiver_email = "receiveremail@gmail.com"
+    password = 'yourpassord'
+    message = """\
+    Subject: {}!
+    
+    This message is sent from Python.""".format(mess)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.ehlo()  # Can be omitted
+        server.starttls(context=context)
+        server.ehlo()  # Can be omitted
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+        server.quit()
+
